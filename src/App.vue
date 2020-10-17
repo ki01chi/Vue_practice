ea6ef0a8a0327c003602f26be86995cfa584a548
chisato <kitada.c08@gmail.com>
2020-10-17 23:57:39 +0900

    modified_vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
     <h1>Hello</h1>
+    <Button></Button>
+    <Button></Button>
+    <Button></Button>
   </div>
 </template>
 
 <script>
+  import Button from './components/button';
 
+  export default {
+    components: {
+      Button
+    }
+  }
 </script>
 
 <style>
---
+++ src/components/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="button">ボタン</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .button {
+    display: inline-block;
+    background-color: red;
+    border-radius: 5px;
+    color: white;
+    padding: 10px;
+    cursor: pointer;
+    margin-right: 5px;
+  }
+
+  .button:hover {
+   background-color: rgb(182, 0, 0); 
+  }
+
+  .button:active {
+    background-color: rgb(138, 13, 13);
+  }
+</style>
